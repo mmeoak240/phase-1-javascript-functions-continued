3f9ee3ac6a3b8688a11134bf35fd475b334ecab2
Michael Meoak <mmeoak240@gmail.com>
2022-02-24 17:01:38 -0700

    commit tree 0c83722d411c701234ab16c8b8f1c4eb311bfb4f

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+
+function saturdayFun(event = "roller-skate") {
+  return `This Saturday, I want to ${event}!`
+}
+
+saturdayFun(bowl);
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`
+}
+
+mondayWork("work from home");
+
+function wrapAdjective(flare) {
+  return function(adj = "special") {
+    return `You are ${flare}${adj}${flare}!`
+
+  }
+}
+
+wrapAdjective("*")("a hard worker");
